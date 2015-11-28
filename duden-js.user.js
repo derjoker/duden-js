085c3200bc2daef5fe5723260784a1f5e82607c6
Ya Feng <fengya09@gmail.com>
2015-11-28 17:19:37 +0800

    prank with jQuery

---
+++ duden-js.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        duden
+// @namespace   fengya
+// @description scrap items as anki card
+// @include     http://www.duden.de/rechtschreibung/*
+// @require		http://code.jquery.com/jquery-2.1.4.js
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+$("h1").hide()
+alert('Hallo duden! Word missing ...');
