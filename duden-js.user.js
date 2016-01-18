9d7700aa2ecc0294445ebb0f1d4d9d392e886d5d
Feng Ya <fengya04@126.com>
2016-01-18 21:45:52 +0800

    version 1.2.0

--- duden-js.user.js
+++ duden-js.user.js
@@ -4,7 +4,7 @@
 // @description scrap items as anki card
 // @include     http://www.duden.de/rechtschreibung/*
 // @require	http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.4.js
-// @version     1.1.1
+// @version     1.2.0
 // @grant       none
 // ==/UserScript==
 
