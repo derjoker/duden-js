4bfacae904eab7255a86b9161b6a4501d16e16e5
Feng Ya <fengya04@126.com>
2016-01-18 21:36:13 +0800

    dblclick

--- duden-dbclick.user.js
+++ duden-dbclick.user.js
@@ -3,12 +3,20 @@
 // @namespace   fengya
 // @description 双击查Duden Online
 // @include     http://www.duden.de/rechtschreibung/*
-// @version     0.0.1
+// @version     0.1.0
 // @grant       none
 // ==/UserScript==
 
-var selection = window.getSelection() ||
-				document.getSelection() ||
-				document.selection.createRange();
-var link = "http://www.duden.de/suchen/dudenonline/" + selection;
-window.open(link);
+'use strict';
+
+window.onload = function() {
+  console.log("window load");
+  document.body.addEventListener("dblclick", function(){
+    var selection = window.getSelection() ||
+    				document.getSelection() ||
+    				document.selection.createRange();
+    var link = "http://www.duden.de/suchen/dudenonline/" + selection;
+    // console.log(link);
+    window.open(link);
+  });
+};
