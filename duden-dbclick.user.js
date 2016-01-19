fb5d2341748fda57e6c7b35c9d87acc785fe390c
Ya Feng <fengya04@126.com>
2016-01-19 11:29:33 +0800

    version 0.2.0
    document.addEventListener("DOMContentLoaded", function() { });
    
    addEventListener
    您可以在文档中添加许多事件，添加的事件不会覆盖已存在的事件。

--- duden-dbclick.user.js
+++ duden-dbclick.user.js
@@ -3,20 +3,20 @@
 // @namespace   fengya
 // @description 双击查Duden Online
 // @include     http://www.duden.de/rechtschreibung/*
-// @version     0.1.0
+// @version     0.2.0
 // @grant       none
 // ==/UserScript==
 
 'use strict';
 
-window.onload = function() {
-  console.log("window load");
-  document.body.addEventListener("dblclick", function(){
+document.addEventListener("DOMContentLoaded", function() {
+  // console.log("DOMContentLoaded.");
+  addEventListener("dblclick", function() {
     var selection = window.getSelection() ||
     				document.getSelection() ||
     				document.selection.createRange();
     var link = "http://www.duden.de/suchen/dudenonline/" + selection;
-    // console.log(link);
+    console.log(link);
     window.open(link);
   });
-};
+});
